d593a78314a506ebee39470e14b4d6e4604cc1a9
19btech <uabbas@19btech.com>
2025-03-23 16:20:39 +0500

    reporting UI fixes

--- src/app/reports/gle-report/page.jsx
+++ src/app/reports/gle-report/page.jsx
@@ -88,8 +88,10 @@ const GLEReportPage = () => {
 
 
   const executeReport = () => {
+    const filteredCriteriaList = criteriaList.filter(criteria => criteria.filters.length > 0);
+    console.log('filters:', filteredCriteriaList);
     const fetchSettings = `${process.env.NEXT_PUBLIC_REPORTING_SERVICE_URI}/jeReport/execute`;
-    axios.post(fetchSettings, criteriaList, {
+    axios.post(fetchSettings, filteredCriteriaList, {
       headers: {
         'X-Tenant': process.env.NEXT_PUBLIC_TENANT,
         Accept: '*/*',
@@ -113,7 +115,6 @@ const GLEReportPage = () => {
 
   // Execute Filter
   const executeFiler = () => {
-    console.log('criteriaList', criteriaList);
     executeReport();
   }
 
@@ -234,187 +235,188 @@ const GLEReportPage = () => {
       </Grid>
 
       <Divider />
-
-      <Box>
-        <Box sx={{ width: '100%', display: 'flex', borderBottom: 1, borderColor: 'divider', alignItems: 'flex-start', margin: 0, padding: 0 }}>
-          <Tabs sx={{ width: '90rem' }} value={0} aria-label="Filter">
-            <Tab label="Filter" sx={{ textTransform: 'none' }} />
-          </Tabs>
-        </Box>
-        <CustomTabPanel value={0} index={0}>
-          <Box sx={{ p: 1 }} justifyContent="center">
-            {/* Render each search criteria */}
-            {criteriaList.map((criteria, index) => (
-              <Box key={index} justifyContent="center" sx={{ mb: 1, border: 1, p: 1, borderRadius: 1 }}>
-                <Grid container spacing={1} alignItems="center" justifyContent="center">
-                  {/* Attribute Selection */}
-                  <Grid xs={12} sm={4} alignItems="center">
-                    <TextField
-                      fullWidth
-                      select
-                      label="Attributes"
-                      value={criteria.attributeName}
-                      onChange={(e) => handleChange(index, "attributeName", e.target.value)}
-                      size="small"
-                      sx={{ minWidth: 350 }}
-                    >
-                      {attributeOptions.length > 0 ? (
-                        attributeOptions.map((option, i) => (
-                          <MenuItem key={i} value={option.attributeName}>
-                            {option.attributeName}
-                          </MenuItem>
-                        ))
-                      ) : (
-                        <MenuItem disabled value="">
-                          No options available
-                        </MenuItem>
-                      )}
-                    </TextField>
-                  </Grid>
-
-                  {/* Operator Selection */}
-                  <Grid xs={12} sm={3}>
-                    <TextField
-                      fullWidth
-                      select
-                      label="Operator"
-                      value={criteria.operator}
-                      onChange={(e) => handleChange(index, "operator", e.target.value)}
-                      disabled={!criteria.attributeName}
-                      size="small"
-                      sx={{ minWidth: 150 }}
-                    >
-                      {criteria.attributeName &&
-                        operatorOptions[attributeOptions.find((t) => t.attributeName === criteria.attributeName)?.dataType] ? (
-                        operatorOptions[
-                          attributeOptions.find((t) => t.attributeName === criteria.attributeName).dataType
-                        ].map((operator, i) => (
-                          <MenuItem key={i} value={operator}>
-                            {operator}
-                          </MenuItem>
-                        ))
-                      ) : (
-                        <MenuItem disabled value="">
-                          Select a column first
-                        </MenuItem>
-                      )}
-                    </TextField>
-                  </Grid>
-
-                  {/* Value Input */}
-                  <Grid xs={12} sm={3}>
-                    <ClickAwayListener onClickAway={() => handleClickAway(index)} key={index}>
-                      <TextField
-                        size="small"
-                        label={`Enter values for Filter ${index + 1}`}
-                        variant="outlined"
-                        value={criteriaList[index].values}
-                        onChange={(e) => handleChange(index, "values", e.target.value)}
-                        onKeyDown={(event) => handleKeyDown(event, index)} // Pass index
-                        fullWidth
-                        sx={{ width: '900px' }}
-                        slotProps={{
-                          input: {
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <Box display="flex" flexDirection="row" flexWrap="nowrap">
-                                  {criteria.filters.map((filter) => (
-                                    <Chip
-                                      key={filter}
-                                      label={filter}
-                                      onDelete={handleDeleteFilter(index, filter)} // Pass criteria index
-                                      style={{ margin: '2px' }}
-                                    />
-                                  ))}
-                                </Box>
-                              </InputAdornment>
-                            ),
-                          },
-                        }}
-                      />
-                    </ClickAwayListener>
-                  </Grid>
-
-                  {/* Remove Criteria Button */}
-                  {criteriaList.length > 1 && (
-                    <Grid xs={12} sm={2}>
-                      <IconButton
-                        onClick={() => handleRemoveCriteria(index)}
-                        aria-label="delete"
-                        size="small"
-                      >
-                        <DeleteOutlineOutlined />
-                      </IconButton>
-                    </Grid>
-                  )}
-                </Grid>
-
-                {/* Second Row: Logical Operator Selection */}
-                {index < criteriaList.length - 1 && (
-                  <Grid container justifyContent="center" sx={{ mt: 1 }}>
-                    <RadioGroup
-                      row
-                      value={criteria.logicalOperator}
-                      onChange={(e) => handleChange(index, "logicalOperator", e.target.value)}
-                      style={{ display: 'flex', alignItems: 'center' }}
-                    >
-                      <div style={{ display: 'flex', alignItems: 'center', marginRight: '16px' }}>
-                        <Radio
-                          value="AND"
-                          size="small"
-                          sx={{
-                            '&.Mui-checked': {
-                              color: '#007BFF', // Change to your desired color
-                            },
-                            '&:hover': {
-                              backgroundColor: 'rgba(0, 123, 255, 0.1)', // Light hover effect
-                            },
-                          }}
-                        />
-                        <span style={{
-                          marginLeft: '8px',
-                          fontSize: '14px', // Reduced font size for a sharper look
-                          color: '#333', // Change to a sharper color
-                          letterSpacing: '0.5px', // Add letter spacing for a sharper look
-                        }}>
-                          AND
-                        </span>
-                      </div>
-                      <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <Radio
-                          value="OR"
-                          size="small"
-                          sx={{
-                            '&.Mui-checked': {
-                              color: '#007BFF', // Change to your desired color
-                            },
-                            '&:hover': {
-                              backgroundColor: 'rgba(0, 123, 255, 0.1)', // Light hover effect
-                            },
-                          }}
-                        />
-                        <span style={{
-                          marginLeft: '8px',
-                          fontSize: '14px', // Reduced font size for a sharper look
-                          color: '#333', // Change to a sharper color
-                          letterSpacing: '0.5px', // Add letter spacing for a sharper look
-                        }}>
-                          OR
-                        </span>
-                      </div>
-                    </RadioGroup>
-                  </Grid>
-                )}
-              </Box>
-            ))}
-          </Box>
-        </CustomTabPanel>
+      <Box sx={{ width: '100%', borderBottom: 1, borderColor: 'divider', alignItems: 'flex-start', margin: 0, padding: 0 }}>
+        <Tabs sx={{ width: '90rem' }} value={0} aria-label="Filter">
+          <Tab label="Filter" sx={{ textTransform: 'none' }} />
+        </Tabs>
       </Box>
+      
 
+      <CustomTabPanel value={0} index={0} >
+        {/* Render each search criteria */}
+        
+        {criteriaList.map((criteria, index) => (
+          <Box key={index} justifyContent="center" sx={{
+            mb: 1, border: 0, p: 1, borderRadius: 1
+          }} >
+            <Grid container spacing={1} alignItems="center" justifyContent="center">
+              {/* Attribute Selection */}
+              <Grid xs={12} sm={4} alignItems="center">
+                <TextField
+                  fullWidth
+                  select
+                  label="Attributes"
+                  value={criteria.attributeName}
+                  onChange={(e) => handleChange(index, "attributeName", e.target.value)}
+                  size="small"
+                  sx={{ minWidth: 350 }}
+                >
+                  {attributeOptions.length > 0 ? (
+                    attributeOptions.map((option, i) => (
+                      <MenuItem key={i} value={option.attributeName}>
+                        {option.attributeName}
+                      </MenuItem>
+                    ))
+                  ) : (
+                    <MenuItem disabled value="">
+                      No options available
+                    </MenuItem>
+                  )}
+                </TextField>
+              </Grid>
+
+              {/* Operator Selection */}
+              <Grid xs={12} sm={3}>
+                <TextField
+                  fullWidth
+                  select
+                  label="Operator"
+                  value={criteria.operator}
+                  onChange={(e) => handleChange(index, "operator", e.target.value)}
+                  disabled={!criteria.attributeName}
+                  size="small"
+                  sx={{ minWidth: 150 }}
+                >
+                  {criteria.attributeName &&
+                    operatorOptions[attributeOptions.find((t) => t.attributeName === criteria.attributeName)?.dataType] ? (
+                    operatorOptions[
+                      attributeOptions.find((t) => t.attributeName === criteria.attributeName).dataType
+                    ].map((operator, i) => (
+                      <MenuItem key={i} value={operator}>
+                        {operator}
+                      </MenuItem>
+                    ))
+                  ) : (
+                    <MenuItem disabled value="">
+                      Select a column first
+                    </MenuItem>
+                  )}
+                </TextField>
+              </Grid>
+
+              {/* Value Input */}
+              <Grid xs={12} sm={3}>
+                <ClickAwayListener onClickAway={() => handleClickAway(index)} key={index}>
+                  <TextField
+                    size="small"
+                    label={`Enter values for Filter ${index + 1}`}
+                    variant="outlined"
+                    value={criteriaList[index].values}
+                    onChange={(e) => handleChange(index, "values", e.target.value)}
+                    onKeyDown={(event) => handleKeyDown(event, index)} // Pass index
+                    disabled={!criteria.operator}
+                    fullWidth
+                    sx={{ width: '900px' }}
+                    slotProps={{
+                      input: {
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <Box display="flex" flexDirection="row" flexWrap="nowrap">
+                              {criteria.filters.map((filter) => (
+                                <Chip
+                                  key={filter}
+                                  label={filter}
+                                  onDelete={handleDeleteFilter(index, filter)} // Pass criteria index
+                                  style={{ margin: '2px' }}
+                                />
+                              ))}
+                            </Box>
+                          </InputAdornment>
+                        ),
+                      },
+                    }}
+                  />
+                </ClickAwayListener>
+              </Grid>
+
+              {/* Remove Criteria Button */}
+              {criteriaList.length > 1 && (
+                <Grid xs={12} sm={2}>
+                  <IconButton
+                    onClick={() => handleRemoveCriteria(index)}
+                    aria-label="delete"
+                    size="small"
+                  >
+                    <DeleteOutlineOutlined />
+                  </IconButton>
+                </Grid>
+              )}
+            </Grid>
+
+            {/* Second Row: Logical Operator Selection */}
+            {index < criteriaList.length - 1 && (
+              <Grid container justifyContent="center" sx={{ mt: 1 }}>
+                <RadioGroup
+                  row
+                  value={criteria.logicalOperator}
+                  onChange={(e) => handleChange(index, "logicalOperator", e.target.value)}
+                  style={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <div style={{ display: 'flex', alignItems: 'center', marginRight: '16px' }}>
+                    <Radio
+                      value="AND"
+                      size="small"
+                      sx={{
+                        '&.Mui-checked': {
+                          color: '#007BFF', // Change to your desired color
+                        },
+                        '&:hover': {
+                          backgroundColor: 'rgba(0, 123, 255, 0.1)', // Light hover effect
+                        },
+                      }}
+                    />
+                    <span style={{
+                      marginLeft: '8px',
+                      fontSize: '14px', // Reduced font size for a sharper look
+                      color: '#333', // Change to a sharper color
+                      letterSpacing: '0.5px', // Add letter spacing for a sharper look
+                    }}>
+                      AND
+                    </span>
+                  </div>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Radio
+                      value="OR"
+                      size="small"
+                      sx={{
+                        '&.Mui-checked': {
+                          color: '#007BFF', // Change to your desired color
+                        },
+                        '&:hover': {
+                          backgroundColor: 'rgba(0, 123, 255, 0.1)', // Light hover effect
+                        },
+                      }}
+                    />
+                    <span style={{
+                      marginLeft: '8px',
+                      fontSize: '14px', // Reduced font size for a sharper look
+                      color: '#333', // Change to a sharper color
+                      letterSpacing: '0.5px', // Add letter spacing for a sharper look
+                    }}>
+                      OR
+                    </span>
+                  </div>
+                </RadioGroup>
+              </Grid>
+            )}
+          </Box>
+        ))}
 
       <div>
         <CustomDataGrid columns={gridHeader} rows={reportData} />
       </div>
+      </CustomTabPanel>
+
     </div>
   );
 };
