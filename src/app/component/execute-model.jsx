4addc4697690ef3854c59635b9680ca113d7330b
19btech <uabbas@19btech.com>
2025-06-11 12:15:29 +0500

    date format change

--- src/app/component/execute-model.jsx
+++ src/app/component/execute-model.jsx
@@ -28,11 +28,12 @@ const ExecuteModel = ({ open, onClose }) => {
     const handleChange = (event) => {
         const value = event.target.value;
         setDate(value);
-    
-        // Validate input format: dd/mm/yyyy
-        const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+
+
+        // Validate input format: MM/dd/yyyy
+        const regex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
         setError(!regex.test(value));
-      };
+    };
 
 
     const handleClose = () => {
@@ -49,9 +50,9 @@ const ExecuteModel = ({ open, onClose }) => {
             setError(true);
             return;
         }
-    
+
         const serviceURL = process.env.NEXT_PUBLIC_SUBLEDGER_SERVICE_URI + '/model/execute';
-    
+
         try {
             console.log('Model to execute:', date);
             const payload = { date: date };
@@ -62,29 +63,29 @@ const ExecuteModel = ({ open, onClose }) => {
                     'Postman-Token': '091bd74b-e836-4185-896a-008fd64b4f46',
                 },
             });
-    
+
             // Handle successful response
             setSuccessMessage(response.data);
             setShowSuccessMessage(true);
-    
+
             setTimeout(() => {
                 setShowSuccessMessage(false);
                 setShowErrorMessage(false);
                 onClose(false);
             }, 3000);
-    
+
         } catch (error) {
             // Handle Axios error
             if (axios.isAxiosError(error)) {
                 console.error('Axios Error:', error);
-    
+
                 if (error.response) {
                     // The request was made and the server responded with a status code
                     // that falls out of the range of 2xx
                     console.error('Response Data:', error.response.data);
                     console.error('Response Status:', error.response.status);
                     console.error('Response Headers:', error.response.headers);
-    
+
                     // Set a user-friendly error message based on the status code
                     if (error.response.status === 500) {
                         setErrorMessage(`Internal Server Error: ${error.response.data.message || '.Please try again later.'}`);
@@ -105,7 +106,7 @@ const ExecuteModel = ({ open, onClose }) => {
                 console.error('Non-Axios Error:', error);
                 setErrorMessage('An unexpected error occurred.');
             }
-    
+
             // Show error message to the user
             setShowErrorMessage(true);
         }
@@ -178,10 +179,10 @@ const ExecuteModel = ({ open, onClose }) => {
                         onChange={handleChange}
                         error={error}
                         helperText={error ? "Invalid date format. Use dd/mm/yyyy." : ""}
-                        placeholder="dd/mm/yyyy"
+                        placeholder="MM/dd/yyyy"
                     />
                     <Typography style={{ align: 'left', alignContent: 'left', fontSize: '12px', color: 'gray' }}>
-                        Execution date of Mode in 'dd/mm/yyyy' format.
+                        Execution date of Mode in 'MM/dd/yyyy' format.
                     </Typography>
                 </Box>
                 <Divider sx={{ marginTop: 3, marginBottom: 2 }} />
